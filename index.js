d3e657f528e3c2077df2ecf30438a650d4b99bec
kolawoleadetola630 <kolawoleadetola630@gmail.com>
2024-09-01 19:20:12 +0000

    updated code

--- index.js
+++ index.js
@@ -16,14 +16,33 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
-
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
   });
 });
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+import { writeFile } from "fs";
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+io.on("connection", (socket) => {
+  socket.on("upload", (file, callback) => {
+    console.log(file); // <Buffer 25 50 44 ...>
+
+    // save the content to the disk, for example
+    writeFile("/tmp/upload", file, (err) => {
+      callback({ message: err ? "failure" : "success" });
+    });
+  });
+});
+
+server.listen(3004, () => {
+  console.log('server running at http://localhost:3004');
 });
